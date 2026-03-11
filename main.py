e2948552b69a5ab64fc74f7f0d568cb5c89217eb
raviraj@parentheses.systems <raviraj@parentheses.systems>
2026-03-11 14:04:02 +0530

    update code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import logging
 from module.humac_driver import humac_driver
+import time
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s | %(levelname)s| %(name)s | %(message)s',
                     datefmt='%H:%M:%S',handlers=[ logging.StreamHandler()])
 
 def main():
     logging.info("HUMAC DRIVER STARTED ...")
+    time.sleep(10)
     humac = humac_driver()
     humac.start() 
     logging.info("HUMAC DRIVER STOPPED ...")
